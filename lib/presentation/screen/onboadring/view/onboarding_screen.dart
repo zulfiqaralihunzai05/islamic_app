e909595448b7bf08e64f9b52bd71f65ec98d7db7
Qadreen_Ali <aliqadreen@gmail.com>
2025-03-27 05:39:42 -0700

    Splash_and on bording screen done

---
+++ lib/presentation/screen/onboadring/view/onboarding_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+import '../../../../dashboard/home-screen.dart';
+
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  _OnboardingScreenState createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 40),
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPage = index;
+                });
+              },
+              children: [
+                _buildPage(
+                  "assets/splash_screen/islamic-pic1.png",
+                  "Find Halal Delights",
+                  "Explore the best local halal restaurants and discover the flavors of the world right at your doorstep.",
+                ),
+                _buildPage(
+                  "assets/splash_screen/islamic-pic2.png",
+                  "Easy Ordering",
+                  "Order your favorite halal meals from nearby restaurants quickly and easily.",
+                ),
+                _buildPage(
+                  "assets/splash_screen/islamic-pic3.png",
+                  "Enjoy Your Meal",
+                  "Get fresh and delicious halal food delivered right to your home.",
+                ),
+              ],
+            ),
+          ),
+           SizedBox(height: 10),
+          // Next Button
+          SizedBox(
+            width: 220,
+            height: 53,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor:  Color(0xFF166534),
+                padding:  EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {
+                if (_currentPage < 2) {
+                  _pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                  );
+                } else {
+
+                  Navigator.push(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+                }
+              },
+              child: Text("Next", style: TextStyle(fontSize: 16, color: Colors.white)),
+            ),
+          ),
+         SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPage(String image, String title, String description) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ClipOval(
+          child: Image.asset(
+            image,
+            width: 250,
+            height: 250,
+            fit: BoxFit.cover,
+          ),
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(3, (index) {
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 4),
+              width: _currentPage == index ? 12 : 8,
+              height: _currentPage == index ? 12 : 8,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _currentPage == index ? Colors.green : Colors.grey,
+              ),
+            );
+          }),
+        ),
+        SizedBox(height: 20),
+        Text(
+          title,
+          style:  TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+            color: Color(0xFF166534), // Dark green
+          ),
+          textAlign: TextAlign.center,
+        ),
+         SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Text(
+            description,
+            style: TextStyle(fontSize: 16, color: Colors.black54),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/presentation/screen/onboadring/view/splash_screen.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+
+import 'onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState()  {
+    super.initState();
+    // Navigate to HomeScreen after 3 seconds
+    Timer( Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) =>  OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 58.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Islam",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  SizedBox(height: 8,),
+                  Text(
+                    "Memorize and recite ",
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400),
+                  ), Text(
+                    "Quran easily",
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 38, left: 18, right: 18),
+            child: Container(
+              width: 314,
+              height: 450,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/splash_screen/islamic-pic1.png"),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(35),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
